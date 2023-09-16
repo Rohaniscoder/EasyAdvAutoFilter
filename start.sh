5f44c89c7b94946d18fb3899f515a2497fec249a
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2023-09-16 13:08:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/spidyynaik/YDNewAuto.git /spidyauto
+  git clone https://github.com/Rohaniscoder/EasyAdvAutoFilter.git /EasyAdvAutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /spidyauto
